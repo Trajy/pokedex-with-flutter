1bb914e677ba59a1e6db548c0959f9510231446c
Henrique Trajano <henrique.araujo@contmatic.com.br>
2023-10-13 19:18:16 -0300

    implemanted basic view layer structure

---
+++ lib/features/home/container/home_container.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_with_flutter/common/models/pokemon_model.dart';
+import 'package:pokedex_with_flutter/common/repository/pokemon_repository_impl.dart';
+import 'package:pokedex_with_flutter/features/home/pages/home_error.dart';
+import 'package:pokedex_with_flutter/features/home/pages/home_loading.dart';
+import 'package:pokedex_with_flutter/features/home/pages/home_page.dart';
+
+class HomeContainer extends StatelessWidget {
+    
+    const HomeContainer({super.key});
+
+    static final PokemonReposotoryImpl reposotory = PokemonReposotoryImpl.instance;
+
+    @override
+    Widget build(BuildContext context) {
+    return FutureBuilder<List<Pokemon>>(
+        future: reposotory.getAllPokemons(),
+        builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting) {
+            return const HomeLoading();
+            }
+            if(snapshot.connectionState == ConnectionState.done && snapshot.hasData) {
+            return HomePage(pokemons: snapshot.data!);
+            }
+            if(snapshot.hasError) {
+            return HomeError(error: snapshot.error.toString());
+            }
+            return const HomeError(error: "Unknown error");
+        }, 
+    );
+}
+}
---
+++ lib/features/home/pages/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:pokedex_with_flutter/common/models/pokemon_model.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key, required this.pokemons});
+
+  final List<Pokemon> pokemons;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
